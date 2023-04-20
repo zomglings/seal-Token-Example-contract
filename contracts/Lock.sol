e282667a313fe66f6d6ab403c5c13c32bdb5ab1c
Roberto  Berwa <berwa05@gmail.com>
2023-04-20 10:19:49 -0400

    changed variable amount to amountWithdrawn

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -8,7 +8,7 @@ contract Lock {
     uint public unlockTime;
     address payable public owner;
 
-    event Withdrawal(uint amount, uint when);
+    event Withdrawal(uint amountWithdrawn, uint when);
 
     constructor(uint _unlockTime) payable {
         require(
